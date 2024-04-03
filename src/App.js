dfb429aa992d72d91cbd47bf656b7fdf6c51647d
Wilson <willythewhale8@gmail.com>
2024-04-03 08:33:15 -0400

    First rendintion of front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [addPhoneState, setAddPhoneState] = useState(false)
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='logo'/>
+      {addPhoneState && <form className='addPhoneState'> 
+          <input type='text'/>
+
+      
+      </form> }
+      <section className='curPhones'>
+      <div className='title'> Current Phones to Add </div>
+      <div className='butSection'>
+        <button className='importBut' onClick={(e)=>{setAddPhoneState(true)}}> Make new Phone </button>
+        <button className='importBut'> Import from CSV </button>  
+      </div>
+      <button className='uploadToServ'> Upload Phones to Server</button>
+      </section>
     </div>
   );
 }
